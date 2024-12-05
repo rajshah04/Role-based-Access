1b457bb6498b4c934085cc58de13b232fc1cb6cd
rajshah04 <rajpshah151@gmail.com>
2024-12-05 17:59:40 +0530

    update cors

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ require("./config/database").connectDB() ;
 app.use(express.json()) ;
 app.use(cookieParser()) ;
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true
 })) ;
 
